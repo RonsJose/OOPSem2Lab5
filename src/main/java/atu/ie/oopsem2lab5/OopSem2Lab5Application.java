1a0078ee3ce501fa8f73b493d48cd78c70eba29f
RonsJose-STUDENT <g00464215@atu.ie>
2026-03-09 11:03:10 +0000

    Initial commit

---
+++ src/main/java/atu/ie/oopsem2lab5/OopSem2Lab5Application.java
@@ -0,0 +1,13 @@
+package atu.ie.oopsem2lab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopSem2Lab5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopSem2Lab5Application.class, args);
+    }
+
+}
---
+++ src/test/java/atu/ie/oopsem2lab5/OopSem2Lab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.oopsem2lab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopSem2Lab5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
