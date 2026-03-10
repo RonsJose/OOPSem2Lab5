3305a24f3d092f998cd817750ed031b6c5c74630
RonsJose-STUDENT <g00464215@atu.ie>
2026-03-10 01:48:50 +0000

    Added checking for conflict with created reservations

--- src/main/java/atu/ie/oopsem2lab5/service/ReservartionService.java
+++ src/main/java/atu/ie/oopsem2lab5/service/ReservartionService.java
@@ -1,5 +1,6 @@
 package atu.ie.oopsem2lab5.service;
 
+import atu.ie.oopsem2lab5.exception.ReservationConflictException;
 import atu.ie.oopsem2lab5.model.Reservation;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,25 @@ public class ReservartionService {
 
     public Reservation addReservation(Reservation reservation) {
         reservation.setReservationID(nextId++);
+        reservations.add(reservation);
+
+        for (Reservation existingReservation : reservations) {
+            if (existingReservation.getEquipmentTag() == reservation.getEquipmentTag() && existingReservation.getReservationDate() == reservation.getReservationDate()) {
+
+                int existingStart = existingReservation.getStartHour();
+                int existingEnd = existingReservation.getDurationHours();
+
+                int newStart = reservation.getStartHour();
+                int newEnd = reservation.getDurationHours();
+
+                if (existingStart < newEnd && newStart < existingEnd) {
+                    reservation.setReservationID(nextId--);
+                    throw new ReservationConflictException("Time slot already booked");
+                }
+
+            }
+        }
+
         reservations.add(reservation);
         return reservation;
     }
